6cf726d62dfc237211c0916c94698e76a3670747
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2016-09-11 16:20:59 -0500

    Trying some mongo stuff

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
+import pymongo
 import configparser
 
 from app import app
+from pymongo import MongoClient
 from flask import (render_template, request, redirect, url_for, session)
 
 config = configparser.ConfigParser()
 config.read("../config/config.cfg")
+
+client = MongoClient()
+
+db = client.test
+entries_collection = db.entries
 
 @app.route("/")
 def hello():
@@ -14,3 +21,11 @@ def hello():
 def name():
     name = config.get("DEFAULT", "name")
     return "<h1 style='color:blue'>Hello, {0}!</h1>".format(name)
+
+@app.route("/test")
+def test():
+    entries = entries_collection.find()[0].get("entries1")
+    html = ""
+    for entry in entries:
+        html = html + "\n<h1 style='color:blue'>Entry: {0}!</h1>".format(int(entry))
+    return html
