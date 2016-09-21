a8823afeed0bb59952276945ed9178a3caf1b1e5
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2016-09-20 22:37:37 -0500

    Add quarterback page pulling from db and add week

--- app/views.py
+++ app/views.py
@@ -10,25 +10,22 @@ config.read("../config/config.cfg")
 
 client = MongoClient()
 
-db = client.test
-entries_collection = db.entries
+week = "Week1"
+# TODO:
+# week = get_current_nfl_weel()
+# formatted "Week1", "Week2", etc
+db = client[week]
+qbs = db.qb
+
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', week=week)
 
 @app.route("/name")
 def name():
     name = config.get("DEFAULT", "name")
     return "<h1 style='color:blue'>Hello, {0}!</h1>".format(name)
-
-@app.route("/test")
-def test():
-    entries = entries_collection.find()[0].get("entries1")
-    html = ""
-    for entry in entries:
-        html = html + "\n<h1 style='color:blue'>Entry: {0}!</h1>".format(int(entry))
-    return html
 
 @app.route("/vote")
 def vote():
@@ -37,3 +34,8 @@ def vote():
 @app.route("/rankings")
 def rankings():
     return render_template('rankings.html')
+
+@app.route("/quarterbacks")
+def quarterbacks():
+    players = qbs.find()
+    return render_template('quarterbacks.html', players=players, week=week)
